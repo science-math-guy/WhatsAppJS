79902437f0670644b0cd7afe8b4ae8f0353bb5ae
science-math-guy <adumont@mailbox.org>
2021-02-26 13:13:54 +0100

    basic frontend

---
+++ frontend/src/Sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Sidebar.css"
+
+function Sidebar() {
+    return (
+        <dir className="sidebar">
+            <h1>Sidebar</h1>
+            <div className="sidebar__header">
+                
+            </div>
+        </dir>
+    )
+}
+
+export default Sidebar
---
+++ frontend/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBCyt2kmCbaPB62xk60PUxGquzo7JYTR0E",
+    authDomain: "whatsappjs-3841e.firebaseapp.com",
+    projectId: "whatsappjs-3841e",
+    storageBucket: "whatsappjs-3841e.appspot.com",
+    messagingSenderId: "810484907979",
+    appId: "1:810484907979:web:49809ca4dfdc4ab8a9804e"
+};
